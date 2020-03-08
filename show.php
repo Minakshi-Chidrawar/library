82cf97279675c5070f9d04117b80b32c24c2a60d
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-08 14:10:54 +0000

    Show book details

---
+++ show.php
@@ -0,0 +1,13 @@
+<?php
+
+$database = require 'core/bootstrap.php';
+
+$id = $_GET['id'];
+
+if (! ($id > 0 && is_numeric($id))) {
+   die("Something went wrong");
+}
+
+$book = $database->book('books', $id);
+
+require 'views/show.view.php';
