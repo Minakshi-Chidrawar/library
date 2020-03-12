fa853794c9d5af2b217782a1e5a18d5a39264242
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-12 20:16:12 +0000

    Added router and request class

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
 $config = require 'config.php';
+
+require 'core/Router.php';
+require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
 
