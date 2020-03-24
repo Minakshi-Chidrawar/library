910731d667bf435a98237a49c428f4150fa9414d
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-24 16:06:35 +0000

    Added route to get an api for all books and single book

--- routes.php
+++ routes.php
@@ -5,6 +5,9 @@ $router->get('show', 'controllers/show.php');
 $router->get('categories', 'controllers/categories.php');
 $router->get('booksByCategory', 'controllers/booksByCategory.php');
 
+$router->get('apibooks', 'controllers/allbooks.php');
+$router->get('book', 'controllers/book.php');
+
 $router->post('update', 'controllers/update.php');
 $router->post('updated', 'controllers/updated.php');
 $router->post('deleteBook', 'controllers/deleteBook.php');
